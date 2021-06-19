380d959bda25b16ad24da9a944f240da40ffdafa
Jibbyworks <JDBucog@yahoo.com>
2021-06-18 18:19:57 -0700

    add more media queries for nav, add nav brand logo animation, please fix self portrait and brand logo on smaller screen sizes

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,23 @@
 const navigatorNav = document.querySelector('#navigator-nav')
+const brandContainer = document.querySelector('#brand-container')
 const navBtn = document.querySelector('#nav-btn')
+const brandLine1 = document.querySelector('.brand1')
+const brandLine2 = document.querySelector('.brand2')
+const brandName = document.querySelector('.brand-name')
 
 navBtn.addEventListener('click', () => {
   navigatorNav.classList.toggle('active')
   navBtn.classList.toggle('active')
+})
+
+brandContainer.addEventListener('mouseenter', function() {
+  brandLine1.classList.add('active')
+  brandLine2.classList.add('active')
+  brandName.classList.add('active')
+})
+
+brandContainer.addEventListener('mouseleave', function() {
+  brandLine1.classList.remove('active')
+  brandLine2.classList.remove('active')
+  brandName.classList.remove('active')
 })

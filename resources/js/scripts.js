8758bb1052e6986859eb642e3d891af3d24bcdf7
Jibbyworks <JDBucog@yahoo.com>
2021-07-13 21:30:48 -0700

    re-edit featured section, removed unused files

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -4,6 +4,13 @@ const navBtn = document.querySelector('#nav-btn')
 const brandLine1 = document.querySelector('.brand1')
 const brandLine2 = document.querySelector('.brand2')
 const brandName = document.querySelector('.brand-name')
+const navLinks = document.querySelectorAll('.nav-link')
+const copyYear = document.querySelector('#year') 
+const projectContainers = document.querySelectorAll('.project-container')
+
+
+//copyright's current year in html
+copyYear.textContent = new Date().getFullYear()
 
 navBtn.addEventListener('click', () => {
   navigatorNav.classList.toggle('active')
@@ -21,3 +28,33 @@ brandContainer.addEventListener('mouseleave', function() {
   brandLine2.classList.remove('active')
   brandName.classList.remove('active')
 })
+
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    navBtn.classList.remove('active')
+    navigatorNav.classList.remove('active')
+  })
+})
+
+projectContainers.forEach(container => {
+  container.addEventListener('mouseenter', () => {
+    let containerImg = container.querySelector('.project-image')
+    let containerCaption = container.querySelector('.project-caption')
+    let overlay = container.querySelector('.overlay')
+    overlay.classList.add('active')
+    containerImg.classList.add('active')
+    containerCaption.classList.add('active')
+  })
+})
+
+projectContainers.forEach(container => {
+  container.addEventListener('mouseleave', () => {
+    let containerImg = container.querySelector('.project-image')
+    let containerCaption = container.querySelector('.project-caption')
+    let overlay = container.querySelector('.overlay')
+    overlay.classList.remove('active')
+    containerImg.classList.remove('active')
+    containerCaption.classList.remove('active')
+  })
+})
+
